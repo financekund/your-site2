7de36b97ea5ebd52a12b328996baa59846ab368c
financekund <financeplanhelp@gmail.com>
2026-04-10 20:58:40 +0530

    site2 upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function scrollToTimer() {
+  document.getElementById("timerSection").scrollIntoView({behavior:"smooth"});
+  startTimer();
+}
+
+function startTimer() {
+  let time = 10;
+  let timer = document.querySelector(".timer");
+
+  let interval = setInterval(() => {
+    time--;
+    timer.innerText = time;
+
+    if (time === 0) {
+      clearInterval(interval);
+      document.getElementById("nextBtn").style.display = "block";
+    }
+  }, 1000);
+}
+
+function goToSite3() {
+  window.location.href = "https://apply.govtyojanaonline.com/verify.html";
+}
